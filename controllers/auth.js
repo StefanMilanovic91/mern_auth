1bec05d768aad9248d98fa786337c7f64e8c6549
Stefan Milanovic <stefan1811991@gmail.com>
2021-04-15 23:42:27 +0200

    Added Login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,8 +21,41 @@ exports.register = async (req, res, next) => {
     }
 };
 
-exports.login = (req, res, next) => {
-    res.send('Login Route');
+exports.login = async (req, res, next) => {
+    
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ success: false, error: "Please provide email and password."});
+    }
+
+    try {
+        const user = await User.findOne({ email }).select('+password');
+
+        // if we don't get user back
+        if (!user) {
+            return res.status(400).json({ success: fasle, error: 'Invalid credentials.' });
+        };
+
+        // check password match
+        const isMatch = await user.matchPasswords(password);
+        // if don't match
+        if (!isMatch) {
+            return res.status(404).json({ success: false, error: 'Invalid credentials.' });
+        };
+
+        // if match
+        res.status(200).json({
+            status: true,
+            token: 'aklwuhdakiuwhdui21uhu3hiubi1uhreuh'
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        })
+    }
 };
 
 exports.forgotpassword = (req, res, next) => {
